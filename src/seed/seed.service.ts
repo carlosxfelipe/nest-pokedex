02ec2948adaee011672ac14a2b90a56ab0cf7f19
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-12-28 16:05:15 -0300

    refactor: integrate PokemonModule into SeedModule and update SeedService for improved seeding functionality

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=151',
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       // console.log(name, url);
 
       const segments = url.split('/');
@@ -19,9 +27,12 @@ export class SeedService {
 
       const no: number = +segments[segments.length - 2];
 
-      console.log(name, no);
+      // console.log(name, no);
+
+      const pokemon = await this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    // return data.results;
+    return 'Seed done!';
   }
 }
